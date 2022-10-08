3ec31b5a91b76cf55fc7ad04472c84369d3629a0
Joel Cedras <joelcedras@gmail.com>
2022-10-08 22:33:14 +0200

    switch to release build

--- authentication_u.pas
+++ authentication_u.pas
@@ -132,10 +132,10 @@ procedure TfrmAuthentication.FormCreate(Sender: TObject);
 begin
   util.setBackground(Self);
 
+  // load header image from assets folder
   imgLogoLong.Picture.LoadFromFile(GetCurrentDir +
     '/assets/logo/logo_long.png');
   imgLogoLong.Proportional := true;
-
 end;
 
 procedure TfrmAuthentication.lblRegisterRedirectClick(Sender: TObject);
